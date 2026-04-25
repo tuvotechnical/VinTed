b250b2121007c49a58c2b454034e812dec5648b0
tuvotechnical <tuvotechnical@users.noreply.github.com>
2026-04-25 09:12:43 +0700

    v1.1.3 - Fix UI clipping in InsertPlus by using SizeToContent

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,8 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("A1B2C3D4-E5F6-7890-ABCD-EF1234567890")]
 
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
+
 
 
 
